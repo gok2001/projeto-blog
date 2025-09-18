c2b1d1e8b100bc8be6b933d179f9ae48718a7316
Guilherme Orige Kernbichler <guilhermeokern@gmail.com>
2025-09-18 19:04:21 -0300

    Corrigir conflitos e ajustes após merges de post-detail e layout

--- posts/urls.py
+++ posts/urls.py
@@ -6,5 +6,5 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
-    path('/posts/<int:post_id>/', views.PostDetail.as_view(), name='post_detail'),
+    path('posts/<int:post_id>/', views.PostDetail.as_view(), name='post_detail'),
 ]
