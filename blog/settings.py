c315b2cdea4fd2b410ab1ce1735a252ee03f1a5f
Guilherme Orige Kernbichler <guilhermeokern@gmail.com>
2025-09-18 15:22:53 -0300

    Recriei o projeto, pois aconteceu algum bug

--- blog/settings.py
+++ blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-54jkrw$@me_(d*l^e-83awi*^t_b1e@2!9(4*d*(nqwftr1g6x'
+SECRET_KEY = 'django-insecure-mz!78=dom-^crl72qely=*wn7mdy#rn1!%l)l@1yx+=(e=6hj1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
