eb38d3a0e11edbc66fed75fa376a9886dc18dda3
Guilherme Orige Kernbichler <guilhermeokern@gmail.com>
2025-09-18 18:19:30 -0300

    Adicionar view Index e template para listar posts

--- posts/models.py
+++ posts/models.py
@@ -5,3 +5,6 @@ class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
     date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from .models import Post
+
+
+class Index(View):
+    post_class = Post
+    template_name = 'posts/post_list.html'
+
+    def get(self, request):
+        posts = self.post_class.objects.all()
+
+        return render(request, self.template_name, {'posts': posts})
