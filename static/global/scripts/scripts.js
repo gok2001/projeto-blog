4a14129041f871f750a7332353f57c312511f605
Guilherme Orige Kernbichler <guilhermeokern@gmail.com>
2025-10-06 23:30:37 -0300

    Refatora JS para toggle genérico e corrige edição de respostas nos templates

--- static/global/scripts/scripts.js
+++ static/global/scripts/scripts.js
@@ -1,41 +1,12 @@
-function toggleReplyForm(commentId) {
-    const form = document.getElementById(`reply-form-${commentId}`);
-    const commentDiv = document.getElementById(`comment-${commentId}`);
-    const buttons = commentDiv.querySelectorAll('.reply-btn, .edit-btn, .delete-btn');
+function toggleForm(formId, parentId) {
+    const form = document.getElementById(formId);
+    const parentDiv = document.getElementById(parentId);
+    const buttons = parentDiv.querySelectorAll('.reply-btn, .edit-btn, .delete-btn');
 
     if (form.style.display === 'none' || form.style.display === '') {
         form.style.display = 'block';
         buttons.forEach(btn => btn.style.display = 'none');
-        
-        const textarea = form.querySelector('textarea');
-        if (textarea) textarea.focus();
 
-        function outsideClickListener(event) {
-            if (!form.contains(event.target) && !Array.from(buttons).includes(event.target)) {
-                form.style.display = 'none';
-                buttons.forEach(btn => btn.style.display = 'inline-block');
-                document.removeEventListener('click', outsideClickListener);
-            }
-        }
-
-        setTimeout(() => {
-            document.addEventListener('click', outsideClickListener);
-        }, 0);
-    } else {
-        form.style.display = 'none';
-        buttons.forEach(btn => btn.style.display = 'inline-block');
-    }
-}
-
-function toggleEditForm(objectId) {
-    const form = document.getElementById(`edit-form-${objectId}`);
-    const commentDiv = document.getElementById(`comment-${objectId}`);
-    const buttons = commentDiv.querySelectorAll('.reply-btn, .edit-btn, .delete-btn')
-
-    if (form.style.display === 'none' || form.style.display === '') {
-        form.style.display = 'block';
-        buttons.forEach(btn => btn.style.display = 'none');
-        
         const textarea = form.querySelector('textarea');
         if (textarea) textarea.focus();
 
